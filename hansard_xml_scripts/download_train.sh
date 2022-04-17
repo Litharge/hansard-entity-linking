68f7768762bdc2eb47f89282ec655383976d7210
Robert Chambers <1robertchambers@gmail.com>
2022-04-17 13:30:47 +0100

    feat: script downloads more training data

--- hansard_xml_scripts/download_train.sh
+++ hansard_xml_scripts/download_train.sh
@@ -6,4 +6,9 @@ data.theyworkforyou.com::parldata/scrapedxml/debates/debates2020-06-16d.xml \
 data.theyworkforyou.com::parldata/scrapedxml/debates/debates2020-06-17e.xml \
 data.theyworkforyou.com::parldata/scrapedxml/debates/debates2020-06-18c.xml \
 data.theyworkforyou.com::parldata/scrapedxml/debates/debates2020-09-11a.xml \
+data.theyworkforyou.com::parldata/scrapedxml/debates/debates2010-11-08a.xml \
+data.theyworkforyou.com::parldata/scrapedxml/debates/debates2010-11-09a.xml \
+data.theyworkforyou.com::parldata/scrapedxml/debates/debates2010-11-10b.xml \
+data.theyworkforyou.com::parldata/scrapedxml/debates/debates2010-11-11b.xml \
+data.theyworkforyou.com::parldata/scrapedxml/debates/debates2010-11-12a.xml \
 hansard_xml/train/
