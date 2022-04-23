0ba926068d8245349a05a4c1db1ef27baea74ad8
Robert Chambers <1robertchambers@gmail.com>
2022-04-23 15:40:28 +0100

    feat: script to create get a list of MPs for a session then iterate over the list to get further info

---
+++ user_scripts/initialise_discourse_model.py
@@ -0,0 +1,14 @@
+# user script to download populate an sqlite DB with MP information for a given session
+
+import sys
+
+# todo: use sys.argv to get cmd line args
+
+from discourse_model.get_list import save_csv
+from discourse_model.create_db import create_db_from_list
+
+
+save_csv("a", "../discourse_model_data/test")
+
+create_db_from_list("../discourse_model_data/test", "../discourse_model_data/mp_db")
+
