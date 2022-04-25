a369687c02bd9139e3629170be3bee309ac58074
Robert Chambers <1robertchambers@gmail.com>
2022-04-25 20:34:53 +0100

    feat: function to detect first person singular pronouns

--- mention_detection/produce_mentions.py
+++ mention_detection/produce_mentions.py
@@ -2,6 +2,7 @@
 
 import stanza
 from lxml import etree
+import re
 
 # iterate through [desired range] of [a given xml file]
 # first determine sentence bounds
@@ -51,6 +52,37 @@ def get_sentence_bounds(nlp: stanza.Pipeline, utt_span: str):
     return sentence_dict
 
 
+def get_first_person_pronouns(utt_span):
+    # bounds of the sections we are interested in, for "I've" this is the first char only
+    first_person_pronoun_spans = {
+        " I ": (1, 2), " I'": (1, 2), " me ": (1, 3), " myself ": (1, 7), " mine ": (1, 5), " my ": (1, 3),
+        " I,": (1, 2), " me,": (1, 3), " myself,": (1, 7), " mine,": (1, 5), " my,": (1, 3),
+        " I\.": (1, 2), " me\.": (1, 3), " myself\.": (1, 7), " mine\.": (1, 5), " my\.": (1, 3),
+        "^I ": (1, 2), "^I'": (1, 2), "^me ": (1, 3), "^myself ": (1, 7), "^mine ": (1, 5), "^my ": (1, 3),
+        "^I\.": (1, 2), "^me\.": (1, 3), "^myself\.": (1, 7), "^mine\.": (1, 5), "^my\.": (1, 3),
+        " I$": (1, 2), " me$": (1, 3), " myself$": (1, 7), " mine$": (1, 5), " my$": (1, 3),
+        "^I$": (1, 2), "^me$": (1, 3), "^myself$": (1, 7), "^mine$": (1, 5), "^my$": (1, 3)
+    }
+
+    spans_found = []
+
+    for key in first_person_pronoun_spans:
+        print(key)
+        for m in re.finditer(key, utt_span, re.IGNORECASE):
+            print("here")
+            spans_found.append(
+                (m.start(0) + first_person_pronoun_spans[key][0],
+                m.start(0) + first_person_pronoun_spans[key][1])
+            )
+
+    return spans_found
+
+
+# get mentions indexed by character [start, end)
+def get_mentions_indexed_by_character(utt_span):
+    get_first_person_pronouns(utt_span)
+
+
 def get_mentions(location, start, end):
     # to put in get_sentences
     nlp = stanza.Pipeline(lang='en', processors='tokenize')
@@ -58,5 +90,11 @@ def get_mentions(location, start, end):
     for utt_span in get_utterance_spans(location, start, end):
         utt_span = transform_hon(utt_span)
         get_sentence_bounds(nlp, utt_span)
+
+        # todo: function to get list of spans of quotations
+
+        get_mentions_indexed_by_character(utt_span)
+
+
 
 
