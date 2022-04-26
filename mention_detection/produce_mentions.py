78f182d84990aade7a4d9e6e411d937f3d43c42e
Robert Chambers <1robertchambers@gmail.com>
2022-04-26 15:54:45 +0100

    fix: comment out planned code

--- mention_detection/produce_mentions.py
+++ mention_detection/produce_mentions.py
@@ -122,20 +122,23 @@ class Mentions():
 
         return repr_str
 
-
+"""
 def get_mentions(location, start, end):
     # to put in get_sentences
-    nlp = stanza.Pipeline(lang='en', processors='tokenize')
+    nlp = stanza.Pipeline(lang='en', processors='tokenize,pos')
+
+    doc = nlp(utt_span)
 
     # goes through each entire utterance span,
     for utt_span in get_utterance_spans(location, start, end):
         utt_span = transform_hon(utt_span)
-        sentence_bounds = get_sentence_bounds(nlp, utt_span)
+
+        sentence_bounds = get_sentence_bounds(doc)
 
         # todo: function to get list of spans of quotations
 
-        mentions = get_first_person_pronouns(utt_span)
+        #mentions = get_first_person_pronouns(utt_span)
 
-        sentence_mentions = Mentions(mentions, sentence_bounds)
-
+        sentence_mentions = Mentions(doc, sentence_bounds)
+"""
 
