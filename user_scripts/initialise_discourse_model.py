efecde5c0e6763c1a44e3cb97c195d4d86d9a180
Robert Chambers <1robertchambers@gmail.com>
2022-04-27 19:51:30 +0100

    feat: change test date

--- user_scripts/initialise_discourse_model.py
+++ user_scripts/initialise_discourse_model.py
@@ -7,8 +7,8 @@ import sys
 from discourse_model.get_list import save_csv
 from discourse_model.create_db import create_db_from_list
 
-test_date = "2017-06-09"
-save_csv("a", "../discourse_model_data/test")
+test_date = "2020-06-15"
+save_csv(test_date, "../discourse_model_data/test")
 
 create_db_from_list("../discourse_model_data/test", "../discourse_model_data/", test_date)
 
