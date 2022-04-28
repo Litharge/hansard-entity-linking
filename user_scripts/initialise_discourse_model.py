5502ad9277e5ddb491445b2b3e2f3afbfa1d304f
Robert Chambers <1robertchambers@gmail.com>
2022-04-28 18:41:29 +0100

    fix: update script to generate pickle

--- user_scripts/initialise_discourse_model.py
+++ user_scripts/initialise_discourse_model.py
@@ -5,10 +5,10 @@ import sys
 # todo: use sys.argv to get cmd line args
 
 from discourse_model.get_list import save_csv
-from discourse_model.create_db import create_db_from_list
+from discourse_model.create_db import MPList
 
 test_date = "2020-06-15"
 save_csv(test_date, "../discourse_model_data/test")
 
-create_db_from_list("../discourse_model_data/test", "../discourse_model_data/", test_date)
+to_save = MPList("../discourse_model_data/test", "../discourse_model_data/", test_date, create_pickle=True)
 
