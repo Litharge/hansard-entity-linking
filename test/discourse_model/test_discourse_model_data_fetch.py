80040378dae1deba889b46c776f6ff019f9b5b50
Robert Chambers <1robertchambers@gmail.com>
2022-04-28 18:15:17 +0100

    test: that discourse model info is retrieved correctly

---
+++ test/discourse_model/test_discourse_model_data_fetch.py
@@ -0,0 +1,27 @@
+import unittest
+import datetime
+
+from discourse_model.create_db import MPList
+
+class TestObjectCreation(unittest.TestCase):
+    def setUp(self):
+        print("here")
+        self.test_obj = MPList(list_file="test", directory="./", date="test", create_pickle=True)
+
+    def test_first_names(self):
+        first_names_only = [x.first_name for x in self.test_obj.mp_list]
+
+        expected = ["Kevin", "Rachel", "Diane"]
+
+        self.assertListEqual(first_names_only, expected)
+
+    def test_last_names(self):
+        last_names_only = [x.last_name for x in self.test_obj.mp_list]
+
+        expected = ["Foster", "Maclean", "Abbott"]
+
+        self.assertListEqual(last_names_only, expected)
+
+    def test_current_offices(self):
+        self.assertTrue(self.test_obj.mp_list[0].current_offices == {'The Parliamentary Under-Secretary of State for the Home Department': datetime.datetime(2019, 12, 16, 0, 0)} )
+
