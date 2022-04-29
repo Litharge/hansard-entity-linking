0fa26f9bbc88d2fff238ab5402d184dfa9d191ba
Robert Chambers <1robertchambers@gmail.com>
2022-04-29 23:03:08 +0100

    feat: module containing function to search text for instances of the regular secretary type of mention

---
+++ mention_detection/regular_secretary.py
@@ -0,0 +1,30 @@
+# recognise "the Home Secretary", "the Work, Retirement and Pensions Secretary" etc.
+
+import re
+
+from mention_detection.annotated_mention import AnnotatedMention
+
+def regex_get_start_end(span, utt_span):
+    spans_found = []
+    for m in re.finditer(span, utt_span):
+        spans_found.append(
+            (m.start(0),
+             m.end(0))
+        )
+
+    return spans_found
+
+
+def get_regular_secretary_mentions(utt_span):
+    # list of triples ((span tuple), rank, shadow)
+    spans_and_attributes = []
+
+    regular_secretary_pattern = r"[Tt]he ([A-Z][a-z]*?)?(( and |, | )([A-Z][a-z]*?)*?)*? Secretary"
+
+    spans = regex_get_start_end(regular_secretary_pattern, utt_span)
+
+    print(spans)
+
+    mentions = [AnnotatedMention(start_char=span[0], end_char=span[1], role="secretary_regular_mention") for span in spans]
+
+    return mentions
