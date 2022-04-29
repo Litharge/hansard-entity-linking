fd1c827d1caff20ede025bdd951bda62ad61350f
Robert Chambers <1robertchambers@gmail.com>
2022-04-29 23:03:40 +0100

    test: that regular secretary mention detection module finds the correct start and end characters

---
+++ test/mention_detection/test_regular_secretary.py
@@ -0,0 +1,24 @@
+import pickle
+from pprint import pprint
+import unittest
+
+from mention_detection.regular_secretary import get_regular_secretary_mentions
+
+from discourse_model.model_from_list import MPList
+
+class TestMemberForSpanDetection(unittest.TestCase):
+    def test_regular_secretary_spans_correctly_identified(self):
+        test_utterance = "The Health Secretary, the Business, Energy and Industrial Strategy Secretary"
+
+        result = get_regular_secretary_mentions(test_utterance)
+
+        for item in result:
+            print(item)
+
+        self.assertTrue((result[0].start_char, result[0].end_char) == (0, 20))
+        self.assertTrue((result[1].start_char, result[1].end_char) == (22, 76))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
