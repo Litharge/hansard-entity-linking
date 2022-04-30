0097ae352b1e82e2f13557515b4c04cecd96e4b1
Robert Chambers <1robertchambers@gmail.com>
2022-04-30 17:41:58 +0100

    feat: module with function to detect irregular noun phrase mentions found during analysis of Hansard

---
+++ mention_detection/irregular_office_mention.py
@@ -0,0 +1,20 @@
+from mention_detection.span_detection import get_regex_span
+
+from mention_detection.annotated_mention import AnnotatedMention
+
+def get_irregular_office_mentions(utt_span):
+    # bounds of the sections we are interested in, for "I've" this is the first char only
+    irregular_spans = [
+        "the Paymaster General",
+        "the Prime Minister",
+        "the Deputy Prime Minister",
+        "the Chancellor",
+        "the Lord Chancellor",
+        "the Leader of the Opposition"
+    ]
+
+    spans = get_regex_span(irregular_spans, utt_span)
+
+    mentions = [AnnotatedMention(start_char=span[0], end_char=span[1], role="irregular_office_mention") for span in spans]
+
+    return mentions
