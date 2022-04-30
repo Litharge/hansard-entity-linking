6b7a1dc0cb6d4e7ce9ecb86cc57ccde08c30126a
Robert Chambers <1robertchambers@gmail.com>
2022-04-30 14:15:01 +0100

    feat: module with function to detect exact nominal mentions

---
+++ mention_detection/exact_nominal_mentions.py
@@ -0,0 +1,31 @@
+import re
+
+from mention_detection.span_detection import get_regex_span
+
+from discourse_model.model_from_list import MPList
+
+from mention_detection.annotated_mention import AnnotatedMention
+
+
+def regex_get_start_end(span, utt_span):
+    spans_found = []
+    for m in re.finditer(span, utt_span):
+        spans_found.append(
+            (m.start(0),
+             m.end(0))
+        )
+
+    return spans_found
+
+
+def get_exact_nominal_mentions(discourse_model: MPList, utt_span):
+    found_spans = []
+
+    for mp in discourse_model.mp_list:
+        exact_nominal = f"{mp.first_name} {mp.last_name}"
+
+        spans_with_associated_entity = regex_get_start_end(exact_nominal, utt_span)
+
+        found_spans.extend([AnnotatedMention(start_char=item[0], end_char=item[1], entity=mp, role="exact_nominal_mention") for item in spans_with_associated_entity])
+
+    return found_spans
