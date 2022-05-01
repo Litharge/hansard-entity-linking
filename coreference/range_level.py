4cd5c25f7095b55ae605baf93133479c5fba0c4e
Robert Chambers <1robertchambers@gmail.com>
2022-05-01 19:59:23 +0100

    feat: generate Mentions for each utterance

--- coreference/range_level.py
+++ coreference/range_level.py
@@ -1,5 +1,9 @@
 from lxml import etree
 
+import stanza
+
+from mention_detection.utterance_level import Mentions
+
 def transform_hon(to_transform):
     return to_transform.replace(" hon. ", " hon  ")
 
@@ -24,8 +28,37 @@ def get_utterance_spans(location, start, end):
 
             utterance_text = transform_hon(utterance_text)
 
-            yield utterance_text
+            yield (utterance_text, ch.get("id"), ch.get("person_id"))
 
         # end the loop when the end pid is found
         if ch.get("id") == end:
             break
+
+
+# class whose instance represents a whole xml document, containing a Mentions for each utterance
+# class will iterate over each Mentions, calling its detect mentions method
+# then it will iterate over each of the AnnotatedMentions in its list of Mentions, calling on the AnnotatedMention its resolution method
+class WholeXMLAnnotation():
+    def __init__(self, xml_location, start, end, model_location, datetime_of_utterance):
+        self.utterance_mentions = {}
+
+        nlp = stanza.Pipeline(lang='en', processors='tokenize,pos')
+
+        for utt_span, utterance_id, person_id in get_utterance_spans(xml_location, start, end):
+            print("\n\nutterance id: ", utterance_id)
+            to_add = Mentions()
+
+            utt_span = transform_hon(utt_span)
+
+            to_add.detect_mentions(nlp, utt_span, model_location, datetime_of_utterance=datetime_of_utterance)
+
+            self.utterance_mentions[utterance_id] = to_add
+
+    def __str__(self):
+        rep = ""
+        for key in self.utterance_mentions:
+            rep += f"==={key}===\n"
+            for mention in self.utterance_mentions[key].annotated_mentions:
+                rep += f"-\n{mention}\n-\n"
+
+        return rep
