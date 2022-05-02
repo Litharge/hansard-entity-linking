7830589838d4ddfbae2ff83a24a6d12f6ea3d562
Robert Chambers <1robertchambers@gmail.com>
2022-05-02 17:41:40 +0100

    test: script to generate model used for testing purposes

---
+++ test/mention_detection/generate_test_model.py
@@ -0,0 +1,14 @@
+# user script to download populate an sqlite DB with MP information for a given session
+
+import sys
+
+# todo: use sys.argv to get cmd line args
+
+from discourse_model.get_list import save_csv
+from discourse_model.model_from_list import MPList
+
+test_date = "2020-06-15"
+save_csv(test_date, "../discourse_model_data/test")
+
+to_save = MPList("../discourse_model_data/test", "../discourse_model_data/", test_date, create_pickle=True)
+
