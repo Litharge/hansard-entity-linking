c2f72a4f9221d0ca5ca74bee63775f28600f5460
Robert Chambers <1robertchambers@gmail.com>
2022-05-04 16:55:39 +0100

    test: whether appositives are joined correctly

--- test/mention_detection/test_utterance_level.py
+++ test/mention_detection/test_utterance_level.py
@@ -127,7 +127,7 @@ class TestProduceMentions(unittest.TestCase):
 
     # test whether multiple mention types can be detected correctly simultaneously
     def test_multiple(self):
-        test_utterance = "I see my hon  friend the shadow Health Secretary agrees. The Parliamentary Under-Secretary of State for the Home Department. Mr Speaker and Madam Deputy Speaker, the member for Redditch. Diane Abbott. The Prime Minister."
+        test_utterance = "I see my hon  friend the shadow Health Secretary agrees, he is right. The Parliamentary Under-Secretary of State for the Home Department. Mr Speaker and Madam Deputy Speaker, the member for Redditch. Diane Abbott. The Prime Minister."
 
         nlp = stanza.Pipeline(lang='en', processors='tokenize,pos')
         m = Mentions()
@@ -139,20 +139,21 @@ class TestProduceMentions(unittest.TestCase):
         for item in m.annotated_mentions:
             print(item)
 
-        sentence_pos_with_features = [(item.sentence_number, item.start_char_in_sentence, item.end_char_in_sentence, item.shadow, item.role, item.get_associated_constituency()) for item in m.annotated_mentions]
+        sentence_pos_with_features = [(item.sentence_number, item.start_char_in_sentence, item.end_char_in_sentence, item.person, item.gender, item.role, item.get_associated_constituency()) for item in m.annotated_mentions]
 
         print(sentence_pos_with_features)
 
-        self.assertTrue((0, 0, 1, None, None, None) in sentence_pos_with_features)
-        self.assertTrue((0, 6, 8, None, None, None) in sentence_pos_with_features)
-        self.assertTrue((0, 6, 20, None, None, None) in sentence_pos_with_features)
-        self.assertTrue((0, 21, 48, True, 'secretary_regular_mention', None) in sentence_pos_with_features)
-        self.assertTrue((1, 0, 66, None, "exact_office_match", None) in sentence_pos_with_features)
-        self.assertTrue((2, 0, 10, None, "speaker_mention", None) in sentence_pos_with_features)
-        self.assertTrue((2, 15, 35, None, "deputy_speaker_mention", None) in sentence_pos_with_features)
-        self.assertTrue((2, 37, 60, None, 'member_for_mention', 'Redditch') in sentence_pos_with_features)
-        self.assertTrue((3, 0, 12, None, 'exact_nominal_mention', 'Hackney North and Stoke Newington') in sentence_pos_with_features)
-        self.assertTrue((4, 0, 18, None, 'irregular_office_mention', None) in sentence_pos_with_features)
+        self.assertTrue((0, 0, 1, 1, None, 'pronominal_mention', None) in sentence_pos_with_features)
+        self.assertTrue((0, 6, 8, 1, None, 'pronominal_mention', None) in sentence_pos_with_features)
+        self.assertTrue((0, 57, 59, 3, 'masculine', 'pronominal_mention', None) in sentence_pos_with_features)
+        self.assertTrue((0, 6, 20, None, 'epicene', 'hon_mention', None) in sentence_pos_with_features)
+        self.assertTrue((2, 0, 10, None, None, 'speaker_mention', None) in sentence_pos_with_features)
+        self.assertTrue((2, 15, 35, None, 'feminine', 'deputy_speaker_mention', None) in sentence_pos_with_features)
+        self.assertTrue((2, 37, 60, None, None, 'member_for_mention', 'Redditch') in sentence_pos_with_features)
+        self.assertTrue((1, 0, 66, None, None, 'exact_office_mention', None) in sentence_pos_with_features)
+        self.assertTrue((3, 0, 12, None, None, 'exact_nominal_mention', 'Hackney North and Stoke Newington') in sentence_pos_with_features)
+        self.assertTrue((0, 21, 48, None, None, 'secretary_regular_mention', None) in sentence_pos_with_features)
+        self.assertTrue((4, 0, 18, None, None, 'irregular_office_mention', None) in sentence_pos_with_features)
 
     # test whether multiple mention types can be detected correctly simultaneously
     def test_overlap_removal(self):
@@ -175,6 +176,43 @@ class TestProduceMentions(unittest.TestCase):
                                       m.annotated_mentions]
 
         print(sentence_pos_with_features)
+
+    def test_apposition_processing(self):
+        # "The Shadow Home Secretary" will be detected both by the exact office match and by the regular secretary match
+        test_utterance = "the Shadow Secretary, the hon  Member for Hackney North and Stoke Newington (Diane Abbott) " \
+                         "is here. My hon  Friend the Member for Redditch (Rachel Maclean) is also here. I see my " \
+                         "hon  friend the shadow Health Secretary agrees, he is right."
+
+        nlp = stanza.Pipeline(lang='en', processors='tokenize,pos')
+        m = Mentions()
+
+        # at this date Diane Abbott was shadow home secretary
+        dummy_datetime = datetime.datetime(2018, 1, 1)
+        m.detect_mentions(nlp, test_utterance, model_location="verified_test_discourse_model.p",
+                          datetime_of_utterance=dummy_datetime)
+        print("unsorted")
+        for item in m.annotated_mentions:
+            print(item)
+
+        m.join_appositives(test_utterance)
+
+        print("sorted")
+        for item in m.annotated_mentions:
+            print(item)
+
+        sentence_pos_with_features = [(item.sentence_number, item.start_char_in_sentence, item.end_char_in_sentence,
+                                       item.shadow, item.role, item.get_associated_constituency(), item.is_appositive) for item in
+                                      m.annotated_mentions]
+
+        print("sentence pos w feats:", sentence_pos_with_features)
+
+        self.assertTrue((0, 0, 90, None, 'member_for_mention', 'Hackney North and Stoke Newington', True) in sentence_pos_with_features)
+        self.assertTrue((1, 0, 2, None, 'pronominal_mention', None, False) in sentence_pos_with_features)
+        self.assertTrue((1, 0, 55, None, 'member_for_mention', 'Redditch', True) in sentence_pos_with_features)
+        self.assertTrue((2, 0, 1, None, 'pronominal_mention', None, False) in sentence_pos_with_features)
+        self.assertTrue((2, 6, 8, None, 'pronominal_mention', None, False) in sentence_pos_with_features)
+        self.assertTrue((2, 6, 48, True, 'secretary_regular_mention', None, True) in sentence_pos_with_features)
+        self.assertTrue((2, 57, 59, None, 'pronominal_mention', None, False) in sentence_pos_with_features)
 
 if __name__ == "__main__":
     unittest.main()
