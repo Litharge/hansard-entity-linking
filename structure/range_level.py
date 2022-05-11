d39b9a1d69b95f4933a7abd2b61a18af5bc9c830
Robert Chambers <1robertchambers@gmail.com>
2022-05-11 19:34:32 +0100

    refactor: place get utterance span inside WholeXMLAnnotation as a method

--- structure/range_level.py
+++ structure/range_level.py
@@ -9,48 +9,48 @@ from structure.utterance_level import Utterance
 def transform_hon(to_transform):
     return to_transform.replace(" hon. ", " hon  ")
 
-def get_utterance_spans(location, start, end):
-    tree = etree.parse(location)
-
-    root = tree.getroot()
-
-    seen_start = False
-
-    for ch in root.getchildren():
-        # skip the loop until the start pid is found
-        if not seen_start and ch.get("id") != start:
-            continue
-        else:
-            seen_start = True
-
-        utterance_text = ""
-        if ch.tag == "speech" and not ch.get("nospeaker") == "true":
-            first_para = True
-            for p in ch.getchildren():
-                if first_para:
-                    first_para = False
-                else:
-                    utterance_text += " "
-                utterance_text += "".join(p.itertext())
-
-            print("utterance text:", utterance_text)
-
-            utterance_text = transform_hon(utterance_text)
-
-            if ch.get("person_id") is not None:
-                yield (utterance_text, ch.get("id"), ch.get("person_id"))
-            else:
-                yield (utterance_text, ch.get("id"), ch.get("speakerid"))
-
-        # end the loop when the end pid is found
-        if ch.get("id") == end:
-            break
-
 
 # class whose instance represents a whole xml document, containing a Utterance for each utterance
 # class will iterate over each Utterance, calling its detect mentions method
 # then it will iterate over each of the AnnotatedMentions in its list of Utterance, calling on the AnnotatedMention its resolution method
 class WholeXMLAnnotation():
+    def get_utterance_spans(self, location, start, end):
+        tree = etree.parse(location)
+
+        root = tree.getroot()
+
+        seen_start = False
+
+        for ch in root.getchildren():
+            # skip the loop until the start pid is found
+            if not seen_start and ch.get("id") != start:
+                continue
+            else:
+                seen_start = True
+
+            utterance_text = ""
+            if ch.tag == "speech" and not ch.get("nospeaker") == "true":
+                first_para = True
+                for p in ch.getchildren():
+                    if first_para:
+                        first_para = False
+                    else:
+                        utterance_text += " "
+                    utterance_text += "".join(p.itertext())
+
+                print("utterance text:", utterance_text)
+
+                utterance_text = transform_hon(utterance_text)
+
+                if ch.get("person_id") is not None:
+                    yield (utterance_text, ch.get("id"), ch.get("person_id"))
+                else:
+                    yield (utterance_text, ch.get("id"), ch.get("speakerid"))
+
+            # end the loop when the end pid is found
+            if ch.get("id") == end:
+                break
+
     def check_if_any_are_secretary(self, offices_to_check):
         for office in offices_to_check:
             if "Secretary of State" in office and not any(
@@ -131,7 +131,7 @@ class WholeXMLAnnotation():
 
         nlp = stanza.Pipeline(lang='en', processors='tokenize,pos')
 
-        for utt_span, utterance_id, person_id in get_utterance_spans(xml_location, start, end):
+        for utt_span, utterance_id, person_id in self.get_utterance_spans(xml_location, start, end):
             #print("utt_span", utt_span)
             #print("\n\nutterance id: ", utterance_id)
             to_add = Utterance(utt_span)
